0fd07ca93398105b7f13e3a7dd883fff5b800c0e
CodeMines <rrai97381@gmail.com>
2025-07-27 17:27:26 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,12 @@
 export default function Footer() {
     return (
-        <>
-            <footer className="footer footer-center bg-base-100 text-base-content p-4">
+        <footer className="footer footer-center bg-base-100 text-base-content p-4">
             <aside>
-               <p>
-  Made by &nbsp;
-  <span className="text-primary underline">Vanitas 👑</span>
-</p>
-
+                <p>
+                    Made by&nbsp;
+                    <span className="text-primary underline">Vanitas 👑</span>
                 </p>
             </aside>
-            </footer>
-        </>
-    )
+        </footer>
+    );
 }
