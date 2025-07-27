e85015bf58758bfa5b8fc1a0794a9ca3aa5f0719
CodeMines <rrai97381@gmail.com>
2025-07-27 17:17:54 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="footer footer-center bg-base-100 text-base-content p-4">
+            <aside>
+               <p>
+  Made by &nbsp;
+  <span className="text-primary underline">Vanitas 👑</span>
+</p>
+
+                </p>
+            </aside>
+            </footer>
+        </>
+    )
+}
